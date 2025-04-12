343c114d7a4b326953ae52167b606f69af11bb7b
camialtr <camialtr.personal@gmail.com>
2025-04-12 16:12:49 -0300

    Video controller half implemented
    - Still needs to fix the sync problem with videoStartTime and the original video length.
    - Fix the bug that stops the video forever when set the slider to the end position.

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -3,6 +3,12 @@ using rec_tool.Models;
 using System.Diagnostics;
 using System.Windows.Threading;
 using LibVLCSharp.Shared;
+using System.Text.Json;
+using System.IO;
+using System.Collections.ObjectModel;
+using Microsoft.Win32;
+
+#pragma warning disable CS8601
 
 namespace rec_tool;
 
@@ -19,6 +25,12 @@ public partial class MainWindow : Window
 
     private LibVLC _libVLC;
     private MediaPlayer _mediaPlayer;
+
+    public ObservableCollection<LittleTest> LittleTestList;
+
+    private MusicTrack _musicTrack;
+
+    private float _videoStartTime;
 
     public MainWindow()
     {
@@ -124,20 +136,121 @@ public partial class MainWindow : Window
 
     private void PlayVideo_Click(object sender, RoutedEventArgs e)
     {
-        VideoView.Visibility = Visibility.Visible;
-        using var media = new Media(_libVLC, @"C:\Users\Administrator\Documents\Repositories\mad.webm");
-        _mediaPlayer.Play(media);
+        if (!_mediaPlayer.IsPlaying)
+        {
+            _mediaPlayer.Play();
+            PlayButton.Content = "Pause Video";
+            TimeSlider.IsEnabled = false;
+            TimeSlider.Opacity = 0.25;
+        }
+        else
+        {
+            _mediaPlayer.Pause();
+            PlayButton.Content = "Play Video";
+            TimeSlider.IsEnabled = true;
+            TimeSlider.Opacity = 1.0;
+        }
     }
 
     private void MediaPlayer_TimeChanged()
     {
+        if (!_mediaPlayer.IsPlaying) return;
+
         try
         {
             TimeLabel.Dispatcher.Invoke(() =>
             {
-                TimeLabel.Content = $"Map Time: {_mediaPlayer.Time / 1000:F2}ms / {_mediaPlayer.Length / 1000:F2}ms";
+                TimeLabel.Content = $"Map Time: {(_mediaPlayer.Time / 1000)}ms / {(_mediaPlayer.Length / 1000) - _videoStartTime}ms";
+                TimeSlider.Value = _mediaPlayer.Time + _videoStartTime;
             });
         }
         catch (Exception) { }
     }
+
+    private void StartRecording_Click(object sender, RoutedEventArgs e)
+    {
+        MusicTrack mt = JsonSerializer.Deserialize<MusicTrack>(File.ReadAllText(@"C:\Users\Administrator\Documents\Testing Projects\Rec Tool\Mad\musictrack.json"));
+
+        LittleTestList = new();
+
+        foreach (int marker in mt.COMPONENTS[0].trackData.structure.markers)
+        {
+            LittleTestList.Add(new() { TestInt = marker, TestString = "AAAAAAAAAAAAAAAAA" });
+        }
+
+        RecordedDataContext.ItemsSource = LittleTestList;
+    }
+
+    private async void Open_Click(object sender, RoutedEventArgs e)
+    {
+        var dialog = new OpenFolderDialog
+        {
+            Title = "Select a project folder",
+        };
+
+        if (dialog.ShowDialog() != true) return;
+
+        var folder = dialog.FolderName;
+
+        if (!File.Exists(@$"{folder}\musictrack.json") || !File.Exists(@$"{folder}\video.webm") || !File.Exists(@$"{folder}\audio.ogg"))
+        {
+            MessageBox.Show("Cannot find all needed files inside this folder.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            return;
+        }
+
+        try
+        {
+            _musicTrack = JsonSerializer.Deserialize<MusicTrack>(File.ReadAllText(@$"{folder}\musictrack.json"));
+        }
+        catch (Exception)
+        {
+            MessageBox.Show("Failed to load musictrack.json.", "Deserialization Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            return;
+        }
+
+        _videoStartTime = float.Parse(_musicTrack.COMPONENTS[0].trackData.structure.videoStartTime.ToString().Replace("-", ""));
+
+        using var media = new Media(_libVLC, @"C:\Users\Administrator\Documents\Testing Projects\Rec Tool\Mad\video.webm");
+        _mediaPlayer.Play(media);
+        VideoView.Visibility = Visibility.Visible;
+        await Task.Delay(100);
+        _mediaPlayer.Pause();
+
+        TimeSlider.Maximum = _mediaPlayer.Length - _videoStartTime;
+        TimeSlider.Minimum = 0;
+
+        TimeLabel.Dispatcher.Invoke(() =>
+        {
+            _mediaPlayer.Time = (long)Convert.ToDouble(_videoStartTime);
+            TimeLabel.Content = $"Map Time: {(_mediaPlayer.Time / 1000)}ms / {(_mediaPlayer.Length / 1000) - _videoStartTime}ms";
+        });
+
+        _mediaPlayer.NextFrame();
+
+        TimeSlider.Opacity = 1.0;
+        TimeSlider.Visibility = Visibility.Visible;
+        TimeSlider.IsEnabled = true;
+        StartRecording.IsEnabled = true;
+        PlayButton.IsEnabled = true;
+        StartServer.IsEnabled = true;
+    }
+
+    private void TimeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+    {
+        if (_mediaPlayer.IsPlaying) return;
+
+        TimeLabel.Dispatcher.Invoke(() =>
+        {
+            _mediaPlayer.Time = (long)Convert.ToDouble(TimeSlider.Value + _videoStartTime);
+            TimeLabel.Content = $"Map Time: {(_mediaPlayer.Time / 1000)}ms / {(_mediaPlayer.Length / 1000) - _videoStartTime}ms";
+        });
+
+        _mediaPlayer.NextFrame();
+    }
+}
+
+public class LittleTest
+{
+    public int TestInt { get; set; }
+    public string TestString { get; set; }
 }
